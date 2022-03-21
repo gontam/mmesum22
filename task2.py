3cf4fd8a0218191baa4ca26788f954c6d281c5e6
Marija <marija.toshevska@students.finki.ukim.mk>
2022-03-21 17:58:00 +0100

    Second task.

--- task2.py
+++ task2.py
@@ -2,7 +2,6 @@ import random
 
 import numpy as np
 
-random_matrix = [[random.random() for e in range(2)] for e in range(3)]
-for i in range(3):
-    for j in range(2):
-        print(random_matrix[i][j])
+random_matrix = [[random.random() for e in range(3)] for e in range(3)]
+
+print(random_matrix)
