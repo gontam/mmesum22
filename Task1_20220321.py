ccd18345525a6e94f7a0b8b4b16d9f110d22326a
jo-hanna22 <johanna.schachl@outlook.de>
2022-03-21 17:45:23 +0100

    Separate branch first commit

--- Task1_20220321.py
+++ Task1_20220321.py
@@ -1,19 +1,23 @@
-### Task 1 - palindrome check
+# Task 1 - palindrome check
 
 # Write a function that checks whether a string is a palindrome
 # Input must be provided by the user in the console
 # Output must be presented in the console
 
-test_str = str(input())
+def is_palindrome():
+    # string input
+    test_str = str(input("Input: "))
 
-# make it suitable for caseless comparison
-test_str = test_str.casefold()
+    # make it suitable for caseless comparison
+    test_str = test_str.casefold()
 
-# reverse the string
-reverse_str = reversed(test_str)
+    # reverse string
+    reverse_str = reversed(test_str)
 
-# check if the string is equal to its reverse
-if list(test_str) == list(reverse_str):
-   print("The string is a palindrome.")
-else:
-   print("The string is not a palindrome.")
+    # check if the string is equal to its reverse
+    if list(test_str) == list(reverse_str):
+        print("The string " + test_str+ " is a palindrome.")
+    else:
+        print("The string " + test_str+ " is not a palindrome.")
+
+is_palindrome()
