ea373df54a138d6fd78099d2a7da28a205641e42
jo-hanna22 <johanna.schachl@outlook.de>
2022-03-21 17:54:23 +0100

    Separate branch first commit

---
+++ Task2_20220321.py
@@ -0,0 +1,14 @@
+# Task 2: Write a script that generates a random matrix with integers
+
+# M = 3
+# N = 3
+# Up to 100
+# Output in the consol
+
+import numpy as np
+
+arr = np.random.randint(0, 100, (5, 4))
+
+print(arr)
+
+
