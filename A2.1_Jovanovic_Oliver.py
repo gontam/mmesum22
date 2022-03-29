64c50c79b5a0f2ce4ad01e65f78da02ae1715043
Oliver Jovanovic <Oliver.jovanovic.p@gmail.com>
2022-03-29 16:26:30 +0200

    Code works, but only last update is saved. ToDo: whole encryption should be written in file.

--- A2.1_Jovanovic_Oliver.py
+++ A2.1_Jovanovic_Oliver.py
@@ -12,15 +12,25 @@ with open('G:\\My Drive\\Universität\\Master\\Master - 2. Semester (SS22) - '
 f.close()
 
 # Read in Dictionary to translate sequence to protein.
-#with open('G:\\My Drive\\Universität\\Master\\Master - 2. Semester (SS22) - '
-          #'Auslandssemester\\Bioinformatics\\standard.txt', 'r') as g:
-    #dt = g.readlines()
-#g.close()
+# with open('G:\\My Drive\\Universität\\Master\\Master - 2. Semester (SS22) - '
+# 'Auslandssemester\\Bioinformatics\\standard.txt', 'r') as g:
+# dt = g.readlines()
+# g.close()
 # Getting information how long the fasta file is.
 lengthFile = len(lines)
-#df = pd.DataFrame(dt)
-#print(df)
-dt = [['TTT', 'TCT', 'TAT', 'TGT', 'TTC', 'TCC', 'TAC', 'TGC', 'TTA', 'TCA', 'TAA', 'TGA', 'TTG', 'TCG', 'TAG', 'TGG', 'CTT', 'CCT', 'CAT', 'CGT', 'CTC', 'CCC', 'CAC', 'CGC', 'CTA', 'CCA', 'CAA', 'CGA', 'CTG', 'CCG', 'CAG', 'CGG', 'ATT', 'ACT', 'AAT', 'AGT', 'ATC', 'ACC', 'AAC', 'AGC', 'ATA', 'ACA', 'AAA', 'AGA', 'ATG', 'ACG', 'AAG', 'AGG', 'GTT', 'GCT', 'GAT', 'GGT', 'GTC', 'GCC', 'GAC', 'GGC', 'GTA', 'GCA', 'GAA', 'GGA', 'GTG', 'GCG', 'GAG', 'GGG'], ['F', 'S', 'Y', 'C', 'F', 'S', 'Y', 'C', 'L', 'S', '*', '*', 'L', 'S', '*', 'W', 'L', 'P', 'H', 'R', 'L', 'P', 'H', 'R', 'L', 'P', 'Q', 'R', 'L', 'P', 'Q', 'R', 'I', 'T', 'N', 'S', 'I', 'T', 'N', 'S', 'I', 'T', 'K', 'R', 'M', 'T', 'K', 'R', 'V', 'A', 'D', 'G', 'V', 'A', 'D', 'G', 'V', 'A', 'E', 'G', 'V', 'A', 'E', 'G'], ['Phe', 'Ser', 'Tyr', 'Cys', 'Phe', 'Ser', 'Tyr', 'Cys', 'Leu', 'Ser', 'Ter', 'Ter', 'Leu', 'Ser', 'Ter', 'Trp', 'Leu', 'Pro', 'His', 'Arg', 'Leu', 'Pro', 'His', 'Arg', 'Leu', 'Pro', 'Gln', 'Arg', 'Leu', 'Pro', 'Gln', 'Arg', 'Ile', 'Thr', 'Asn', 'Ser', 'Ile', 'Thr', 'Asn', 'Ser', 'Ile', 'Thr', 'Lys', 'Arg', 'Met', 'Thr', 'Lys', 'Arg', 'Val', 'Ala', 'Asp', 'Gly', 'Val', 'Ala', 'Asp', 'Gly', 'Val', 'Ala', 'Glu', 'Gly', 'Val', 'Ala', 'GLu', 'Gly']]
+# df = pd.DataFrame(dt)
+# print(df)
+dt = [['TTT', 'TCT', 'TAT', 'TGT', 'TTC', 'TCC', 'TAC', 'TGC', 'TTA', 'TCA', 'TAA', 'TGA', 'TTG', 'TCG', 'TAG', 'TGG',
+       'CTT', 'CCT', 'CAT', 'CGT', 'CTC', 'CCC', 'CAC', 'CGC', 'CTA', 'CCA', 'CAA', 'CGA', 'CTG', 'CCG', 'CAG', 'CGG',
+       'ATT', 'ACT', 'AAT', 'AGT', 'ATC', 'ACC', 'AAC', 'AGC', 'ATA', 'ACA', 'AAA', 'AGA', 'ATG', 'ACG', 'AAG', 'AGG',
+       'GTT', 'GCT', 'GAT', 'GGT', 'GTC', 'GCC', 'GAC', 'GGC', 'GTA', 'GCA', 'GAA', 'GGA', 'GTG', 'GCG', 'GAG', 'GGG'],
+      ['F', 'S', 'Y', 'C', 'F', 'S', 'Y', 'C', 'L', 'S', '*', '*', 'L', 'S', '*', 'W', 'L', 'P', 'H', 'R', 'L', 'P',
+       'H', 'R', 'L', 'P', 'Q', 'R', 'L', 'P', 'Q', 'R', 'I', 'T', 'N', 'S', 'I', 'T', 'N', 'S', 'I', 'T', 'K', 'R',
+       'M', 'T', 'K', 'R', 'V', 'A', 'D', 'G', 'V', 'A', 'D', 'G', 'V', 'A', 'E', 'G', 'V', 'A', 'E', 'G'],
+      ['Phe', 'Ser', 'Tyr', 'Cys', 'Phe', 'Ser', 'Tyr', 'Cys', 'Leu', 'Ser', 'Ter', 'Ter', 'Leu', 'Ser', 'Ter', 'Trp',
+       'Leu', 'Pro', 'His', 'Arg', 'Leu', 'Pro', 'His', 'Arg', 'Leu', 'Pro', 'Gln', 'Arg', 'Leu', 'Pro', 'Gln', 'Arg',
+       'Ile', 'Thr', 'Asn', 'Ser', 'Ile', 'Thr', 'Asn', 'Ser', 'Ile', 'Thr', 'Lys', 'Arg', 'Met', 'Thr', 'Lys', 'Arg',
+       'Val', 'Ala', 'Asp', 'Gly', 'Val', 'Ala', 'Asp', 'Gly', 'Val', 'Ala', 'Glu', 'Gly', 'Val', 'Ala', 'GLu', 'Gly']]
 # Not the first line, because there is information about the dataset in it, which is not important to us.
 x = 1
 str1 = ""
@@ -35,21 +45,6 @@ while x <= lengthFile - 1:
 # Convert List in String.
 str1 = str1.join(listNew)
 
-# Convert list to string
-#x = 1
-#str2 = ""
-#listNew = []
-#while x <= len(dt) - 1:
-#    string = dt[x]
-#    string = string.strip('\n')
-#    listNew += string
-#    print('Dict line', x, 'processed')
-#    x += 1
-
-#str2 = str2.join(listNew)
-
-
-
 # Splitting String to analyze genome.
 split_strings = []
 n = 3
@@ -58,7 +53,48 @@ for index in range(0, len(str1), n):
 print(split_strings[0])
 
 print('The whole genome is:', str1)
-#print('The whole list is:', str2)
 print('The whole list is:', dt)
 # Delete unnecessary variables to save space.
-del x, string, listNew, lengthFile, f, g, lines, dt, n, index
+del x, string, listNew, lengthFile, f, lines, n, index
+
+# Implementing Logic
+v = int(input("W: "))
+a = []
+j = 0
+i = 0
+if v == 1:
+    while i <= len(split_strings):
+        if len(split_strings[i]) == 3:
+            if split_strings[i] == dt[0][j]:
+                print(split_strings[i], '=', dt[0][j])
+                a = dt[1][j]
+                with open('encryption.txt', 'w') as f:
+                    f.write(dt[1][j])
+                i += 1
+                j = 0
+            elif j == 64:
+                break
+                f.close()
+            else:
+                j += 1
+        else:
+            break
+            f.close()
+
+if v == 2:
+    while i <= len(split_strings):
+        if len(split_strings[i]) == 3:
+            if split_strings[i] == dt[0][j]:
+                print(split_strings[i], '=', dt[0][j])
+                with open('encryption.txt', 'w') as f:
+                    f.write(dt[1][j])
+                i += 1
+                j = 0
+            elif j == 64:
+                break
+                f.close()
+            else:
+                j += 1
+        else:
+            break
+            f.close()
