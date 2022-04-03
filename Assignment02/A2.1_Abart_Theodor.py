7e526b31e56f99e51b458c5114b8ef4b120765eb
T Abart <theodor.abart@gmail.com>
2022-04-03 23:34:06 +0200

    temp_1

---
+++ Assignment02/A2.1_Abart_Theodor.py
@@ -0,0 +1,23 @@
+def read_fasta(filename):
+    f = open(filename, 'r')
+    data = f.readlines()
+    data = data[1::]  # remove first line
+    data = ' '.join([str(line) for line in data])
+    return data
+
+
+def read_standard(filename):
+    f = open(filename, 'r')
+    data = f.readlines()
+    for i in range(data.__len__()):
+        data[i] = data[i].split()
+    return data
+
+
+filename_sequence = 'BD137219.1.fasta'
+data = read_fasta(filename_sequence)
+# print(data)
+
+filename_standard = 'standard.txt'
+standard = read_standard(filename_standard)
+print(standard)
