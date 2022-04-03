259b9ed21dfef171d7906dd4d9c0b0d4727c9dc4
Andrey-Ukhnalyov-20 <andrey_ukhnalev20@mail.ru>
2022-04-03 15:21:05 +0200

    Solving the task A2.2 | Error with the instability index

--- A2/A2.2_Ukhnalyov_Andrey.py
+++ A2/A2.2_Ukhnalyov_Andrey.py
@@ -3,14 +3,47 @@
 
 from Bio import Entrez
 from Bio import SeqIO
+from Bio.SeqUtils.ProtParam import ProteinAnalysis
 
 import csv
 
+gens = ["OCA2", "PAX3", "EDNRB", "SOX10", "MITF"]
+
+def search_in_db(topics: list[str], entries_number: int, data_base: str) -> list[list[any]]:
+    array_to_write = []
+
+    for topic in topics:
+        search = Entrez.esearch(term=topic, retmax=entries_number, db=data_base)
+        search_results = Entrez.read(search)
+        search.close()
+
+        for element in search_results['IdList']:
+            data_fetch = Entrez.efetch(id=element, db=data_base, rettype="gb", retmode="text")
+            data_fetch_results = SeqIO.read(data_fetch, "genbank")
+            data_fetch.close()
+
+            analysis_results = ProteinAnalysis(str(data_fetch_results.translate()))
+            row_to_write = [data_fetch_results.id, data_fetch_results.description, data_fetch_results.annotations["organism"], len(data_fetch_results.seq),
+                              analysis_results.get_amino_acids_percent()['G'], 'analysis_results.instability_index() -> error happened', analysis_results.aromaticity(), analysis_results.isoelectric_point()]
+            
+            array_to_write.append(row_to_write)
+
+    return array_to_write
+
 
 def main():
-    pass
+    Entrez.email = "me21m018@technikum-wien.at"
+
+    with open("A2.2_Heterochromia_Ukhnalyov_Andrey.csv", "w") as file:
+        csv_table = csv.writer(file)
+        csv_table.writerow(["Accession number", "Title", "Organism", "Sequence length", "CG %", "Protein instability", "Aromaticity", "Isoelectric point"])
+
+        rows = search_in_db(gens, 5, "nucleotide")
+
+        for row in rows:
+            csv_table.writerow(row)
 
 
 if __name__ == '__main__':
-    pass
+    main()
 
