fd492161cd2c51877deeb926cbe8b6cf2261a746
soricris <sorianocristina1c@yahoo.es>
2022-04-13 14:01:34 +0200

    first try

--- Assignment_3/Assignment_3_K-Means.py
+++ Assignment_3/Assignment_3_K-Means.py
@@ -1,1 +1,15 @@
 # This is a test.
+
+import csv
+file = open('input.csv')
+print(file)
+data = []
+for i in file:
+    data.append(i)
+
+cluster_num = data[0][3]
+
+data[1]=data[1].split(';')
+rows = data[1][0]
+columns = data[1][1]
+
