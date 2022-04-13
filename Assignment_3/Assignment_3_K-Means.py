f30c324f1abf7944bdb0e4b5be903d89a723ca5d
Oliver Jovanovic <Oliver.jovanovic.p@gmail.com>
2022-04-13 21:12:39 +0200

    Added ToDo for Cristina Soriano. Included HTTP-Links for help.

--- Assignment_3/Assignment_3_K-Means.py
+++ Assignment_3/Assignment_3_K-Means.py
@@ -1,5 +1,9 @@
-# This is a test.
 
+# Read in Data (Cristina Soriano):
+# TODO: Read in Data with pandas (as Dataframe): see: https://datatofish.com/import-csv-file-python-using-pandas/
+# TODO: Please delete the character (\n) from data set.
+# TODO: As you can see in, e.g. data[2] you have two values in one string please do two columns e.g., https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html
+# TODO: Make the data look like the table in excel that I uploaded in our folder.
 file = open('input.csv')
 print(file)
 data = []
