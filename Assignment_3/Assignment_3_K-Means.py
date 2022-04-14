806031b10e4e6dff9a6965390c65e4070bf1e202
Oliver Jovanovic <Oliver.jovanovic.p@gmail.com>
2022-04-14 12:19:58 +0200

    Ordered the ToDo's.

--- Assignment_3/Assignment_3_K-Means.py
+++ Assignment_3/Assignment_3_K-Means.py
@@ -4,10 +4,12 @@ import pandas as pd
 from sklearn.cluster import KMeans
 
 # Read in Data (Cristina Soriano):
-# TODO: Read in Data with pandas (as Dataframe): see: https://datatofish.com/import-csv-file-python-using-pandas/
-# TODO: Please delete the character (\n) from data set.
-# TODO: As you can see in, e.g. data[2] you have two values in one string please do two columns e.g., https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html
-# TODO: Make the data look like the table in excel that I uploaded in our folder.
+# ToDo: Read in Data with pandas (as Dataframe): see:
+#  https://datatofish.com/import-csv-file-python-using-pandas/
+# ToDo: Please delete the character (\n) from data set.
+# ToDo: As you can see in, e.g. data[2] you have two values in one string please do two columns e.g.,
+#  https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html
+# ToDo: Make the data look like the table in excel that I uploaded in our folder.
 file = open('input.csv')
 print(file)
 data = []
@@ -16,12 +18,12 @@ for i in file:
 
 cluster_num = data[0][3]
 
-data[1]=data[1].split(';')
+data[1] = data[1].split(';')
 rows = data[1][0]
 columns = data[1][1]
 
-# Implementation of Algorithm k-means (Oliver Jovanovic):
+# Implementation of Algorithm k-means (Oliver Jovanović):
 # ToDo: Implement the algorithm using the given data.
-# ToDo: Process the data as most efficient and saveable for Marija Toshevska.
+# ToDo: Process the data as most efficient and usable for Marija Toshevska.
 
-# Store Values in output_example.csv (new one).
+# Store Values in output_example.csv (new one) (Marija Toshevska).
