d953abb49f396483134484912517d0b8be53d85a
Oliver Jovanovic <Oliver.jovanovic.p@gmail.com>
2022-04-17 13:33:14 +0200

    Add URL.

--- Assignment_3/Assignment_3_K-Means.py
+++ Assignment_3/Assignment_3_K-Means.py
@@ -10,20 +10,24 @@ from sklearn.cluster import KMeans
 # ToDo: As you can see in, e.g. data[2] you have two values in one string please do two columns e.g.,
 #  https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html
 # ToDo: Make the data look like the table in excel that I uploaded in our folder.
-file = open('input.csv')
-print(file)
-data = []
-for i in file:
-    data.append(i)
 
-cluster_num = data[0][3]
 
-data[1] = data[1].split(';')
-rows = data[1][0]
-columns = data[1][1]
+#file = open('input.csv')
+#print(file)
+#data = []
+#for i in file:
+#    data.append(i)
+
+#cluster_num = data[0][3]
+
+#data[1] = data[1].split(';')
+#rows = data[1][0]
+#columns = data[1][1]
 
 # Implementation of Algorithm k-means (Oliver Jovanović):
 # ToDo: Implement the algorithm using the given data.
+# ToDo: Implement it from scratch:
+#  https://www.analyticsvidhya.com/blog/2021/04/k-means-clustering-simplified-in-python/
 # ToDo: Process the data as most efficient and usable for Marija Toshevska.
 
 # Store Values in output_example.csv (new one) (Marija Toshevska).
