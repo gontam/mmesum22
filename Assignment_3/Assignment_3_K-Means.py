c0574ea31905f16bb8f20928040a5ac6ae36304e
Oliver Jovanovic <Oliver.jovanovic.p@gmail.com>
2022-04-18 16:03:00 +0200

    Casted dat points from string to float and Plotted them.

--- Assignment_3/Assignment_3_K-Means.py
+++ Assignment_3/Assignment_3_K-Means.py
@@ -1,5 +1,5 @@
 # Import libraries:
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import os
 import csv
 import numpy as np
@@ -53,8 +53,9 @@ del rows[0]
 
 # Carve out Data points:
 data_points = rows
-
 x = 0
+x_point = 0
+y_point = 0
 while x <= data_length - 1:
     lst = data_points[x]
     splits = np.array_split(lst, dimensions)
@@ -68,6 +69,7 @@ while x <= data_length - 1:
 
 # Change "," in "."
 x = 0
+lst = []
 while x <= len(x_point) - 1:
     a = x_point[x]
     b = y_point[x]
@@ -78,7 +80,21 @@ while x <= len(x_point) - 1:
     x_point[x] = a
     y_point[x] = b
     x += 1
+
+# Cast every string into float
+x_point = list(map(float, x_point))
+y_point = list(map(float, y_point))
+
 del rows, splits, data_points, x, a, b, lst, file
+
+# Plot the points:
+plt.scatter(y_point, x_point, alpha=.5)
+plt.xlim([0, 1])
+plt.ylim([0, 1])
+plt.xlabel('X-Values')
+plt.ylabel('Y-Values')
+plt.title('Have a look at the dat points')
+plt.show()
 
 # Implementation of Algorithm k-means (Oliver Jovanović):
 # ToDo: Implement the algorithm using the given data.
