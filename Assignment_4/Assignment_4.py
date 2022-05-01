f674fc05cf41901c682dabb7a71aaf21c4953d98
Oliver Jovanovic <Oliver.jovanovic.p@gmail.com>
2022-05-01 13:58:56 +0200

    Started Assignemnt 4. Have a look how many entries are for APOL1 and download 5 different entries.

---
+++ Assignment_4/Assignment_4.py
@@ -0,0 +1,21 @@
+# Handed in by Oliver Jovanović
+# Import Libraries:
+from Bio import Entrez
+
+
+# Chose gene 'APOL1' from Assignment 1:
+# Have a look how many results are there for 'APOL1':
+Entrez.email = 'me21x506@technikum-wien.at'
+handle = Entrez.egquery(term='APOL1')
+record = Entrez.read(handle)
+for row in record['eGQueryResult']:
+    if row['DbName'] == 'nuccore':
+        print(row['Count'])
+handle.close()
+# 7060 Results!
+
+# Download five different entries:
+handle = Entrez.esearch(db='nucleotide', term='APOL1', retmax=5, idtype='acc')
+record = Entrez.read(handle)
+handle.close()
+
