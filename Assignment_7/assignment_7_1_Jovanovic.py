6b3c0450801c1570f5c75ba95411f6278bb53da7
Oliver Jovanovic <Oliver.jovanovic.p@gmail.com>
2022-05-23 13:46:29 +0200

    Assignment_7_1 Setup.

---
+++ Assignment_7/assignment_7_1_Jovanovic.py
@@ -0,0 +1,13 @@
+# LU Linear equation solver:
+# @Author: Jovanović
+# Import necessary libraries:
+import numpy as np
+
+# Lower triangular system:
+def Lsub(L, b):
+    pass
+# Upper triangular system:
+def Usub(U, y):
+    pass
+
+
