0bf90e50fae54167d1eb9e569682f4e0907be1aa
lisa0703 <102049590+lisa0703@users.noreply.github.com>
2022-05-29 14:10:04 +0200

    Add files via upload

--- A5_Stefely_Lisa.py
+++ A5_Stefely_Lisa.py
@@ -28,9 +28,10 @@
 
 
 #example
-# small village with 2000 people, after first infections full lockdown
+# small village with 2000 people, after first infections people are not allowed to leave the village
 
-
+#Tried to change the formulas of https://mdpi-res.com/d_attachment/mathematics/mathematics-09-00636/article_deploy/mathematics-09-00636.pdf?version=1615969616
+# accordnigly (without quarantine, newborns, normal deaths, etc. -> therefore alternative version of the shown equations in the paper)
 
 import numpy as np
 from scipy.integrate import odeint
@@ -49,10 +50,10 @@ beta, gamma = 1, 0.25 #beta typical values: 0.05 to 1.0; gamma: 1/9 to 1/4
 eps = 1/5 #average 1/5.2 -> rounded to 1/5
 alpha = 0.0001 #Fatality rate: between 0.000005 and 0.0001
 # A grid of time points (in days)
-t = np.linspace(0, 40)
+t = np.linspace(0, 100)
 
 
-# The SIR model differential equations.
+# The SEIR model differential equations.
 def deriv(y, t, N, beta, gamma, eps, alpha):
     S, I, R, E, D = y
     dSdt = -(beta * S * I) / N
@@ -80,7 +81,7 @@ ax.plot(t, D / 100, 'm', alpha=0.5, lw=2, label='Dead')
 ax.set_title('SEIR Model: Covid in a small village with lockdown')
 ax.set_xlabel('Time /days')
 ax.set_ylabel('Number (100s)')
-ax.set_ylim(0, 20)
+ax.set_ylim(0, 21)
 ax.yaxis.set_tick_params(length=0)
 ax.xaxis.set_tick_params(length=0)
 ax.grid(visible=True, which='major', c='w', lw=2, ls='-')
@@ -90,7 +91,7 @@ for spine in ('top', 'right', 'bottom', 'left'):
     ax.spines[spine].set_visible(False)
 plt.show()
 
-# with intervention of vaccination -> example 10 vaccinations per day starting on t=0
+# with intervention of vaccination
 
 # Total population, N.
 N = 2000
@@ -98,33 +99,38 @@ N = 2000
 I0, R0 = 10, 0
 D0 = 0
 E0 = 30
+V0 = 0
 # Everyone else, S0, is susceptible to infection initially.
 S0 = N - I0 - R0 - D0 - E0
 # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-beta, gamma = 1, 0.25 #beta typical values: 0.05 to 1.0; gamma: 1/9 to 1/4
-eps = 1/5 #average 1/5.2 -> rounded to 1/5
+beta, gamma = 1/N, 0.25 #beta typical values: 0.05 to 1.0; gamma: 1/9 to 1/4
+eps = 1/5 # Time until infected; average 1/5.2 -> rounded to 1/5
 alpha = 0.0001 #Fatality rate: between 0.000005 and 0.0001
+vac = 0.2 #Vaccination rate
+sigma = 0.8#efficiency of vaccination
 # A grid of time points (in days)
-t = np.linspace(0, 40)
-V=10
+t = np.linspace(0, 100)
+#V=10
 
+#change alpha to vac
 
 # The SEIR model differential equations.
-def deriv(y, t, N, beta, gamma, eps, alpha):
-    S, I, R, E, D = y
-    dSdt = -(beta * S * I) / N -V
-    dEdt = (beta * S *I) /N - eps * E
+def deriv(y, t, N, beta, gamma, eps, alpha, vac, sigma):
+    S, I, R, E, D, V = y
+    dSdt = -(beta * S * I) - vac * S
+    dEdt = (beta * S *I) - eps * E + beta * sigma * V * I
     dIdt = eps * E - (gamma + alpha) * I
-    dRdt = gamma * I + V
+    dRdt = gamma * I
     dDdt = alpha * I
-    return dSdt, dIdt, dRdt, dEdt, dDdt
+    dVdt = vac * S - sigma * beta * V * I
+    return dSdt, dIdt, dRdt, dEdt, dDdt, dVdt
 
 
 # Initial conditions vector
-y0 = S0, I0, R0, E0, D0
+y0 = S0, I0, R0, E0, D0, V0
 # Integrate the SIR equations over the time grid, t.
-ret = odeint(deriv, y0, t, args=(N, beta, gamma, eps, alpha))
-S, I, R, E, D= ret.T
+ret = odeint(deriv, y0, t, args=(N, beta, gamma, eps, alpha, vac, sigma))
+S, I, R, E, D, V= ret.T
 
 # Plot the data on three separate curves for S(t), I(t) and R(t)
 fig = plt.figure(facecolor='w')
@@ -132,12 +138,13 @@ ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
 ax.plot(t, S / 100, 'b', alpha=0.5, lw=2, label='Susceptible')
 ax.plot(t, I / 100, 'r', alpha=0.5, lw=2, label='Infected')
 ax.plot(t, E / 100, 'y', alpha=0.5, lw=2, label='Exposed')
-ax.plot(t, R / 100, 'g', alpha=0.5, lw=2, label='Recovered with immunity (including Vaccinations -> 10/day)')
+ax.plot(t, R / 100, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
 ax.plot(t, D / 100, 'm', alpha=0.5, lw=2, label='Dead')
+ax.plot(t, V / 100, 'c', alpha=0.5, lw=2, label='Vaccinated')
 ax.set_title('SEIR Model: Covid in a small village with lockdown (with vaccinations)')
 ax.set_xlabel('Time /days')
 ax.set_ylabel('Number (100s)')
-ax.set_ylim(0, 20)
+ax.set_ylim(0, 21)
 ax.yaxis.set_tick_params(length=0)
 ax.xaxis.set_tick_params(length=0)
 ax.grid(visible=True, which='major', c='w', lw=2, ls='-')
