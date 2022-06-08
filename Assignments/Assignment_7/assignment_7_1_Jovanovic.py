51a2101b92ce5370074c30736b839daaec0b01c3
Oliver Jovanovic <Oliver.jovanovic.p@gmail.com>
2022-06-08 14:33:37 +0200

    Quicksort implemented.

--- Assignments/Assignment_7/assignment_7_1_Jovanovic.py
+++ Assignments/Assignment_7/assignment_7_1_Jovanovic.py
@@ -1,5 +1,6 @@
 # LU Linear equation solver:
 # @Author: Jovanović
+# Based on: https://www.youtube.com/watch?v=UlWcofkUDDU&ab_channel=Mathispower4u
 # Import necessary libraries:
 import numpy as np
 import sys
@@ -11,57 +12,62 @@ col = int(col)
 print('Please give a number for rows:')
 row = input()
 row = int(row)
+tmp = 0.0
+
+# Compute U by given L.
+def compute(A, L):
+    k = 1
+    l = 0
+    m = 0
+    if A[row - k, l] == A[0, l]:
+        pass
+    else:
+        if A[row - k, l] < 0:
+            if A[0, l] > A[row - k, l]:
+                tmp = A[row - k, l] / A[0, l]
+                L[row - k, l] = tmp
+                for y in range(l, row - 1):
+                    A[row - k, l] = A[row - k, l] + tmp * A[0, l]
+                    print(A)
+                    print(L)
+            elif A[0, l] < A[row - k, l]:
+                tmp = A[0, l] / A[row - k, l]
+                L[row - k, l] = tmp
+                for y in range(l, row - 1):
+                    A[row - k, l] = A[row - k, l] + tmp * A[0, l]
+                    print(A)
+                    print(L)
+
+        elif A[row - k, l] > 0:
+            if A[0, l] > A[row - k, l]:
+                tmp = A[row - k, l] / A[0, l]
+                L[row - k, l] = tmp
+                for y in range(l, row - 1):
+                    A[row - k, l] = A[row - k, l] - tmp * A[0, l]
+                    print(A)
+                    print(L)
+            elif A[0, l] < A[row - k, l]:
+                tmp = A[0, l] / A[row - k, l]
+                L[row - k, l] = tmp
+                for y in range(l, row - 1):
+                    A[row - k, l] = A[row - k, l] - tmp * A[0, l]
+                    print(A)
+                    print(L)
+
+
 
 # Randomized Matrix:
-N = np.random.rand(row, col)
-
+A = np.random.rand(row, col)
 # Checking if rows and columns are equal:
 if row == col:
     # If Columns and Rows are equal, do the LU decomposition.
-    print("It is a quadratic matrix!")
-    print("The current quadratic Matrix looks like:", N)
-    Lsub(L, b)
-
+    print("The quadratic Matrix A looks like:\n", A)
+    L = np.eye(row)
+    print("Matrix L looks like:\n", L)
+    compute(A, L)
 else:
     # If Columns and Rows are unequal, exit the script.
     print("It is not a quadratic matrix. The Program will be shut down.")
     sys.exit(0)
 
-# Lower triangular system:
-def Lsub(L, b):
-    L = N
-    b = np.ones(row, dtype="float")
 
-# Upper triangular system:
-def Usub(U, y):
-    pass
-
-def lu(A, piv=True, Tol=10 ** (-7)):
-    Nr, Nc = A.shape  # Number of rows and columns
-    if Nr != Nc:
-        print('Matrix is not square')
-        return None
-    N = Nr
-    permu = np.array(range(N))  # Initialize permutation vector p
-    for j in range(N - 1):
-        p_val = abs(A[j, j])
-        p_ind = j
-        for i in range(j + 1, N):
-            if abs(A[i, j]) > p_val:
-                p_val = abs(A[i, j])
-                p_ind = i
-        if p_val < Tol:
-            print('Matrix nearly degenerate (tolereance = ' + str(Tol) + ').')
-            return None
-        if piv:
-            # Row pivoting, i.e. put row with largest element in current
-            # column on top
-            permu[j], permu[p_ind] = permu[p_ind], permu[j]
-            A[[j, p_ind], :] = A[[p_ind, j], :]
-
-        for i in range(j + 1, N):
-            A[i, j] = A[i, j] / A[j, j]  # A[i,j] now stores L[i,j]
-            for k in range(j + 1, N):
-                A[i, k] = A[i, k] - A[i, j] * A[j, k]
-            # A[i,j+1:N] = A[i,j+1:N] - A[i,j]*A[j,j+1:N] #faster assignment
-    return A, permu
---
+++ Assignments/Assignment_7/assignment_7_2_Jovanovic.py
@@ -0,0 +1,106 @@
+# Author: Oliver Jovanović
+# Quicksort
+import numpy as np
+import random as rnd
+import matplotlib.pyplot as plt
+from time import time
+
+def partition(low, high, arr):
+    pivot = arr[high]
+    pointer = low
+    for i in range(low, high):
+        if arr[i] <= pivot:
+            arr[i], arr[pointer] = arr[pointer], arr[i]
+            pointer += 1
+    arr[pointer], arr[high] = arr[high], arr[pointer]
+    return pointer
+
+
+def quicksort(low, high, arr):
+    if len(arr) == 1:  # Terminating Condition for recursion. VERY IMPORTANT!
+        return arr
+    if low < high:
+        pi = partition(low, high, arr)
+        quicksort(low, pi - 1, arr)  # Recursively sorting the left values
+        quicksort(pi + 1, high, arr)  # Recursively sorting the right values
+    return arr
+
+
+def myquicksort(lst):
+    quicksort(0, len(lst) - 1, lst)
+    print(lst)
+
+
+# By Ramberger:
+def check_my_sort():
+    # general initialization
+    N_pts = 5  # number of different lentghs for which sort time is measured
+    Nt = 10  # number of repetitions to measure time
+    results = np.zeros([2, N_pts, 2])
+
+    # range of lengths for build in method
+    Emin = 5  # log10 of minimal sample size
+    Emax = 6  # log10 of maximal sample size
+    pts = np.linspace(Emin, Emax, N_pts)  # contains exponents for list lengths
+    for i in range(N_pts):
+        N = int(10 ** (pts[i]))  # lentgh of list
+        num_list = np.zeros(N)
+        print(N)
+        # initialization for timing of list with length N
+        for j in range(N):
+            num_list[j] = rnd.uniform(0, 100)
+        # timing for built in method
+        t1 = time()
+        for t in range(Nt):
+            sorted_num_list = np.sort(num_list, kind='mergesort')
+        t2 = time()
+        # write time for each list length in results
+        results[0, i, 0] = N
+        results[0, i, 1] = (t2 - t1) / Nt
+
+    # range of lengths for build in method
+    Emin = 2  # log10 of minimal sample size
+    Emax = 3  # log10 of maximal sample size
+    pts = np.linspace(Emin, Emax, N_pts)  # contains exponents for list lengths
+    for i in range(N_pts):
+        N = int(10 ** (pts[i]))  # lentgh of list
+        num_list = np.zeros(N)
+        print(N)
+        # initialization for timing of list with length N
+        for j in range(N):
+            num_list[j] = rnd.uniform(0, 100)
+        # timing for myquicksort
+        t1 = time()
+        for t in range(Nt):
+            sorted_num_list = myquicksort(num_list)
+        t2 = time()
+        # write time for each list length in results
+        results[1, i, 0] = N
+        results[1, i, 1] = (t2 - t1) / Nt
+
+    print(results[0, :, :])
+    print(results[1, :, :])
+    x1 = np.log10(results[0, :, 0])
+    y1 = np.log10(results[0, :, 1])
+    x2 = np.log10(results[1, :, 0])
+    y2 = np.log10(results[1, :, 1])
+    k1, d1 = np.polyfit(x1, y1, 1)
+    k2, d2 = np.polyfit(x2, y2, 1)
+
+    plt.figure(0)
+    plt.plot(x1, y1, 'x')
+    plt.plot(x1, k1 * x1 + d1, '-')
+    plt.ylabel('log(t)')
+    plt.xlabel('log(N)')
+    print('Slope and intercept of built-in in log-log plot are ' + str(k1)
+          + ' and ' + str(d1))
+
+    plt.figure(1)
+    plt.plot(x2, y2, 'x')
+    plt.plot(x2, k2 * x2 + d2, '-')
+    print('Slope and intercept of myquicksort in log-log plot are ' + str(k2)
+          + ' and ' + str(d2))
+    plt.show()
+
+check_my_sort()
+
